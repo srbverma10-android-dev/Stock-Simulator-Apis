956baf28ca99e04c78e3f2360064d2cfd470df45
SOURABH KUMAR VERMA <1906286@kiit.ac.in>
2021-10-21 13:12:47 +0530

    minimising SECURE_HSTS_SECONDS

--- stockSimulatorApi/stockSimulatorApi/settings.py
+++ stockSimulatorApi/stockSimulatorApi/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_SECONDS = 3
 
 SecurityMiddleware = True
 
