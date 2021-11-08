b8ea75eb082ce47fd546caed8ab6f524478256fd
SOURABH KUMAR VERMA <1906286@kiit.ac.in>
2021-11-08 23:30:32 +0530

    returning top gainer and looser with most traded

--- stockSimulatorApi/bhavcopydev/views.py
+++ stockSimulatorApi/bhavcopydev/views.py
@@ -1,9 +1,9 @@
 from datetime import date, timedelta
 
+from nsepy.history import get_price_list
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from nsepy.history import get_price_list
 
 
 @api_view(['GET'])
@@ -18,16 +18,24 @@ def getBhavCopy(request):
             except:
                 i = i + 1
         try:
-            prices = prices.drop(['SERIES', 'ISIN', 'TIMESTAMP'], axis=1)
+            prices = prices.drop(
+                ['SERIES', 'ISIN', 'TIMESTAMP', 'HIGH', 'LOW', 'OPEN', 'LAST', 'TOTTRDQTY', 'TOTTRDVAL'], axis=1)
+            listOfChange = []
+            for index, row in prices.iterrows():
+                change = (((row['CLOSE'] - row['PREVCLOSE']) * 100) / row['CLOSE'])
+                listOfChange.append(change)
+            prices['CHANGE'] = listOfChange
             context = {
-                'data': prices,
+                'top_gainer': prices.sort_values(by='CHANGE', ascending=False).head(20).drop(['TOTALTRADES', 'PREVCLOSE'], axis=1),
+                'top_looser': prices.sort_values(by='CHANGE', ascending=True).head(20).drop(['TOTALTRADES', 'PREVCLOSE'], axis=1),
+                'most_traded': prices.sort_values(by='TOTALTRADES', ascending=False).head(20).drop(['TOTALTRADES', 'PREVCLOSE'], axis=1),
                 'hasNext': False,
                 'code': status.HTTP_200_OK
             }
-        except:
+        except Exception as e:
             context = {
-                'data': "",
+                'data': str(e),
                 'hasNext': False,
-                'code': status.HTTP_200_OK
+                'code': status.HTTP_400_BAD_REQUEST
             }
         return Response(context, status=status.HTTP_200_OK)
